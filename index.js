e5db2797892d61b1f0e4b27fdb10bb9ccab39b32
Vishal-Gori <95775403+Vishal-Gori@users.noreply.github.com>
2023-06-19 18:30:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ app.get("/find", (req, res) => {
     })
     res.json({"msg":msg})
 });
-app.listen(5000, () => {console.log("ready @5000")})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {console.log("ready @5000")})
